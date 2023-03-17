1e5da74b9dd0cc5e3ffedcff4f9af55199b7f474
irannaozuna2599@gmail.com <irannaozuna2599@gmail.com>
2023-03-17 18:14:21 -0400

    Agregar front login y registro de usuarios.

---
+++ PartShop/PartShop/Pages/Registro_usuarios.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PartShop.Pages
+{
+    public class Registro_usuariosModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}

582a9ab51b99d50c86c7c3b8619afc8893121662
AnibalCruz <anibal17918@gmail.com>
2023-03-17 01:15:41 -0400

    USER ADMINISTRATION

---
+++ PartShop/PartShop.Infrastructure/Repositories/UsuarioRepository.cs
@@ -0,0 +1,12 @@
+﻿using PartShop.Domain.Entities;
+using PartShop.Domain.Repositories;
+
+namespace PartShop.Infrastructure.Repositories
+{
+    public class UsuarioRepository : BaseRepository<Usuario>, IUsuarioRepository
+    {
+        public UsuarioRepository(PartShopDbContext context) : base(context)
+        {
+        }
+    }
+}

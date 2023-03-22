201e9da594392ea9ce72c754e44500dca7d91ec0
AnibalCruz <anibal17918@gmail.com>
2023-03-22 01:33:17 -0400

    Corrección de campos en registro usuario

--- PartShop/PartShop/Pages/Registro_usuarios.cshtml.cs
+++ PartShop/PartShop/Pages/Registro_usuarios.cshtml.cs
@@ -1,12 +1,26 @@
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
 
-namespace PartShop.Pages
+namespace PartShop.Pages.Registro_usuarios
 {
-    public class Registro_usuariosModel : PageModel
+    public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly PartShop.Infrastructure.PartShopDbContext _context;
+
+        public IndexModel(PartShop.Infrastructure.PartShopDbContext context)
         {
+            _context = context;
+        }
+
+        public IList<Domain.Entities.Usuario> Usuarios { get; set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Usuario != null)
+            {
+                Usuarios = await _context.Usuario
+                .Include(p => p.IdRolNavigation).ToListAsync();
+            }
         }
     }
 }

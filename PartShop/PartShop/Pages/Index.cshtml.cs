ff820b8813070fc445111dec8478cd330d0a424e
AnibalCruz <anibal17918@gmail.com>
2023-03-22 20:32:18 -0400

    Search

--- PartShop/PartShop/Pages/Index.cshtml.cs
+++ PartShop/PartShop/Pages/Index.cshtml.cs
@@ -1,1 +1,37 @@
-﻿
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using PartShop.Domain.Entities;
+
+namespace PartShop.Pages;
+
+public class IndexModel : PageModel
+{
+    private readonly PartShop.Infrastructure.PartShopDbContext _context;
+
+    public IndexModel(PartShop.Infrastructure.PartShopDbContext context)
+    {
+        _context = context;
+    }
+
+    public IList<Domain.Entities.Producto> Producto { get; set; } = default!;
+
+    public void OnGet()
+    {
+        Producto = new List<Domain.Entities.Producto>();
+    }
+
+    public async Task OnPostAsync()
+    {
+        var searchString = Request.Form["searchString"];
+        if (!string.IsNullOrEmpty(searchString))
+        {
+            Producto = await _context.Producto
+                .Where(p => p.Nombre.Contains(searchString)).ToListAsync();
+        }
+        else
+        {
+            ModelState.AddModelError("searchString", "Please enter a search term.");
+        }
+    }
+
+}

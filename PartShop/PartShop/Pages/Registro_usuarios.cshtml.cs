f18fdfe1a9df2f6228c0b9de72227e1be7ef4227
AnibalCruz <anibal17918@gmail.com>
2023-03-23 16:52:35 -0400

    LogIn and Register BACKEND done

--- PartShop/PartShop/Pages/Registro_usuarios.cshtml.cs
+++ PartShop/PartShop/Pages/Registro_usuarios.cshtml.cs
@@ -1,5 +1,9 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartShop.Domain.Entities;
+using System.Threading.Tasks;
 
 namespace PartShop.Pages.Registro_usuarios
 {
@@ -12,8 +16,10 @@ namespace PartShop.Pages.Registro_usuarios
             _context = context;
         }
 
-        public IList<Domain.Entities.Usuario> Usuarios { get; set; } = default!;
+        [BindProperty]
+        public Usuario Usuario { get; set; }
 
+        public IList<Domain.Entities.Usuario> Usuarios { get; set; } = default!;
         public async Task OnGetAsync()
         {
             if (_context.Usuario != null)
@@ -22,5 +28,30 @@ namespace PartShop.Pages.Registro_usuarios
                 .Include(p => p.IdRolNavigation).ToListAsync();
             }
         }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            var usuario = new Usuario
+            {
+                Nombres = Usuario.Nombres,
+                Apellidos = Usuario.Apellidos,
+                Correo = Usuario.Correo,
+                Usuario1 = Usuario.Usuario1,
+                Clave = Usuario.Clave,
+                IdTienda = Usuario.IdTienda,
+                IdRol = Usuario.IdRol
+            };
+
+            _context.Usuario.Add(usuario);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("/Index");
+        }
     }
+
 }
